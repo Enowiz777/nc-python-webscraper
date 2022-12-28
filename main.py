046094d5ad94425b570be0558ce9fa6129405a06
Enoch Park <enoch.park@enowiz.com>
2022-12-27 19:58:37 -0800

    20221227

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from extractors.wwr import extract_wwr_jobs
 
 keyword = input("What do you want to search for?")
+wwr_jobs = extract_wwr_jobs(keyword)
+
 
 file = open(f"{keyword}".csv, "w")
 
-file.write("Position,Company,Location,URL")
+file.write("Position,Company,Location,URL\n")
+
+for job in wwr_jobs:
+    file.write(
+        f"{job['position']}, {job['company']}, {job['location']},{job['link']}\n"
+    )
 file.close()
 
 """
